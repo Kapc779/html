e2a4fcbb6203ebc46ed2fff482d752d965223ccd
Kapc779 <70428992+Kapc779@users.noreply.github.com>
2020-12-18 07:00:47 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ let toggler = document.querySelector(".toggler");
 let fullscreen = document.querySelector(".fullscreen");
 
 toggler.addEventListener('click', () => {
-    fullscreen.classList.toggle("dark");
+    fullscreen.classList.toggle("light");
 })
